e0cc98ca3678d378515cf405e4a94e56eada0c12
LaudOak <1047627381@qq.com>
2015-10-16 22:23:58 +0800

    :octocat:

---
+++ app/src/main/java/me/laudoak/oakpark/activity/OakParkActivity.java
@@ -0,0 +1,80 @@
+package me.laudoak.oakpark.activity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.view.View;
+
+import me.laudoak.oakpark.R;
+
+/**
+ * Created by LaudOak on 2015-10-16 at 21:46.
+ */
+public class OakParkActivity extends XBaseActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void setView() {
+        setContentView(R.layout.activity_oakpark);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_oakpark_drawer);
+        initDrawer();
+    }
+
+    /*build drawer layout & pager*/
+    private void initDrawer()
+    {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    public void buildView() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+}
---
+++ app/src/main/java/me/laudoak/oakpark/fragment/DrawerFragment.java
@@ -0,0 +1,28 @@
+package me.laudoak.oakpark.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by LaudOak on 2015-10-16 at 22:03.
+ */
+public class DrawerFragment extends XBaseFragment {
+
+    private static final String TAG = "DrawerFragment";
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public View callView(LayoutInflater inflater, ViewGroup container) {
+        return null;
+    }
+
+    @Override
+    public void buildViews(View view) {
+
+    }
+}
