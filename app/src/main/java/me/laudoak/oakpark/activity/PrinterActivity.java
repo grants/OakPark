8c5c46db66894e619391f26a473b287466983f6c
LaudOak <1047627381@qq.com>
2015-11-07 08:49:12 +0800

    :octocat:

---
+++ app/src/main/java/me/laudoak/oakpark/activity/PrinterActivity.java
@@ -0,0 +1,18 @@
+package me.laudoak.oakpark.activity;
+
+/**
+ * Created by LaudOak on 2015-11-6 at 22:46.
+ */
+public class PrinterActivity extends XBaseActivity {
+
+
+    @Override
+    protected void setView() {
+
+    }
+
+    @Override
+    public void buildView() {
+
+    }
+}
