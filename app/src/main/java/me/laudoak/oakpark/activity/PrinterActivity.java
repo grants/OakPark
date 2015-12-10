f12863a371cc6b3fe0f844d371f73449a245c957
LaudOak <1047627381@qq.com>
2015-12-10 13:59:13 +0800

    :octocat:

--- app/src/main/java/me/laudoak/oakpark/activity/PrinterActivity.java
+++ app/src/main/java/me/laudoak/oakpark/activity/PrinterActivity.java
@@ -215,18 +215,18 @@ public class PrinterActivity extends XBaseActivity implements
         oks.disableSSOWhenAuthorize();
 
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        oks.setTitle("分享来自橡树园的一首诗");
+        oks.setTitle(title.getText().toString());
         // text是分享文本，所有平台都需要这个字段
-        oks.setText(title.getText().toString()+"\n"+author.getText().toString()+"\n"+verse.getText().toString());
+        oks.setText(title.getText().toString() + "\n" + author.getText().toString() + "\n" + verse.getText().toString());
 
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
         oks.setImagePath(filname);//确保SDcard下面存在此张图片
 
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
+        //oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
         oks.setSite(getString(R.string.app_name));
-        oks.setUrl("https://github.com/");
+        //oks.setUrl("https://github.com/");
 
         // 启动分享GUI
         oks.show(this);
